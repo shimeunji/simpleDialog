40130eaefd0d1a0953531316917b4b555a71e6e1
Eunji Shim <eunji991222@gmail.com>
2016-06-07 20:28:39 +0900

    #3 Setting EventHandler

--- app/src/main/java/kr/hs/emirim/kokopink1/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kokopink1/simpledialog/MainActivity.java
@@ -1,10 +1,13 @@
 package kr.hs.emirim.kokopink1.simpledialog;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
@@ -26,7 +29,12 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         dialog.setTitle("First Dialog");
         dialog.setMessage("This is message part.");
         dialog.setIcon(R.drawable.first_icon);
-        dialog.setPositiveButton("확인", null);
+        dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog,int which) {
+                Toast.makeText(MainActivity.this,"대화상자의 확인버튼을 클릭했음",Toast.LENGTH_LONG).show();
+            }
+        });
         dialog.show();
     }
 }
