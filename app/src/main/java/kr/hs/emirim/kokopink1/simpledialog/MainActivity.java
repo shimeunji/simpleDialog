43a3e3771ed6251807d5daffe2ce75dfeea9eac1
Eunji Shim <eunji991222@gmail.com>
2016-06-07 20:07:10 +0900

    first commit

--- app/src/main/java/kr/hs/emirim/kokopink1/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kokopink1/simpledialog/MainActivity.java
@@ -1,13 +1,32 @@
 package kr.hs.emirim.kokopink1.simpledialog;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but = (Button) findViewById(R.id.but_dialog);
+        but.setOnClickListener(this);
+    }
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(getApplicationContext());
+        dialog.setTitle("First Dialog");
+        dialog.setMessage("This is message part.");
+        dialog.setIcon(R.drawable.first_icon);
+        dialog.setPositiveButton("확인", null);
+        dialog.show();
     }
 }
